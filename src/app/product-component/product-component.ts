b41f9d1229f2865903d3c4f92731f7605a939dd4
Nicolas <nicolas.pupo56@gmail.com>
2025-09-02 11:02:01 -0300

    Criação de objeto, tabela e função de salvar e limpar formulário

---
+++ src/app/product-component/product-component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { product } from '../product';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-component',
+  standalone: false,
+  templateUrl: './product-component.html',
+  styleUrl: './product-component.css'
+})
+export class ProductComponent {
+  products: product[] = [];
+  formGroupProduct: FormGroup;
+
+  constructor(private formBuilder : FormBuilder){
+    this.formGroupProduct = this.formBuilder.group({
+      id: [''],
+      name: [''],
+      description: [''],
+      price: ['']
+    });
+  }
+
+  save() {
+    this.products.push(this.formGroupProduct.value);
+
+  }
+}
