e141b137d4a36145a01c07289f252b23ced34f30
Nicolas Pupo <nicolas.pupo56@gmail.com>
2025-09-01 21:48:42 -0300

    primeiro commit

---
+++ src/app/product/product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: false, //Obrigatóriamente o componente é criado dentro de um módulo.
+  templateUrl: './product.html',
+  styleUrl: './product.css'
+})
+export class Product {
+
+}
